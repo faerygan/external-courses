a0105d1d8c1e4ca68f7ada15bb55caad0d6bab1c
epam <epam@local.rsu.edu.ru>
2018-10-05 20:55:05 +0300

    Shults testing 2...

--- src/ex2_js-basics-part2/Task_02.js
+++ src/ex2_js-basics-part2/Task_02.js
@@ -1,6 +1,9 @@
 var customArray = [54, 'null', 12, '54', 'js', true, true];
 var i = 0;
-for (i = 0; i < customArray.length; ++i) {
-    console.log ('Element ', i+1, ': ', customArray[i]);
+var arrLength = function (customArray) {
+    for (i = 0; i < customArray.length; ++i) {
+        console.log ('Element', i+1, ':', customArray[i]);
+    }
+    console.log ('Total:', customArray.length, 'elements.');
 }
-console.log ('Total: ', customArray.length, 'elements.');
+arrLength (customArray);
